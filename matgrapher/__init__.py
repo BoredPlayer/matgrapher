2b9a30b1bdc51db359f38c57cb5ccc730a501f72
Stanisław Gajek <stanislaw.gajek@vp.pl>
2022-01-15 12:25:11 +0100

    Prepared files for pip installation.

---
+++ matgrapher/__init__.py
@@ -0,0 +1,7 @@
+"""
+matgrapher is a library intended to provide functions limiting steps necessary to create a matplotlib graph.
+"""
+
+
+__version__ = "0.1.0"
+__author__ = "BoredPlayer"
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='matgrapher',
+    version = '0.1.0',
+    description = 'An easy to use python class aiding with matplotlib graph making.',
+    url = 'https://github.com/BoredPlayer/matgrapher',
+    author = 'BoredPlayer',
+    author_email = 'oskar.g.rpi@vp.pl',
+    license = 'MIT License',
+    packages = ['matgrapher'],
+    install_requires = ['numpy', 'matplotlib'],
+    
+    classifiers=[
+        'Developements Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
