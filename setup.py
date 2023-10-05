3794251838fdf09901cf1041bd46f27f894d5f4d
BoredPlayer <oskar.g.rpi@vp.pl>
2023-10-05 15:30:23 +0200

    Updated setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='matgrapher',
-    version = '0.2.4',
+    version = '0.2.5',
     description = 'An easy to use python class aiding with matplotlib graph making.',
     url = 'https://github.com/BoredPlayer/matgrapher',
     author = 'BoredPlayer',
